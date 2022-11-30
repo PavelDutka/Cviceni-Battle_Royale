591dcac29a5b0b7ba6a088ed69e0420a254813a0
PavelDutka <71589947+PavelDutka@users.noreply.github.com>
2022-11-30 13:53:08 +0100

    Add files via upload

---
+++ Cviceni-Battle_Royale/Cviceni-Battle_Royale/Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_Battle_Royale
+{
+    class Enemy
+    {
+    }
+
+    class Fighter : Enemy
+    {
+       public string Name { get; set; }
+    }
+
+    class Knight : Fighter
+    {
+
+    }
+
+    class Sorcerer
+    {
+
+    }
+    class Beast
+    {
+        Paralyze()
+        {
+
+        }
+    }
+}
