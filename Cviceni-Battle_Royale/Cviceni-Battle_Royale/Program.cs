1f3b794fcda03ecadf212e01459d8e18b10d4e4c
PavelDutka <pavel.dutka05gmail.com>
2022-12-06 12:50:01 +0100

    .

--- Cviceni-Battle_Royale/Cviceni-Battle_Royale/Program.cs
+++ Cviceni-Battle_Royale/Cviceni-Battle_Royale/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("");
+﻿using CsC04Battle;
+
+Enemy e1 = new Enemy(1);
+Enemy e2 = new Enemy(2);
+Enemy[] combatants = new Enemy[] { e1, e2 };
+int combAlive = combatants.Length;
+int round = 0;
+while (combAlive > 1)
+{
+    Console.WriteLine("-- " + round++ + " --");
+    foreach (var c in combatants)
+    {
+        var x = c.PickOpponent(combatants);
+        c.Attack(2, x);
+    }
+    combAlive = 0;
+    foreach (var c in combatants)
+    {
+        if (c.IsAlive) combAlive++;
+        Console.WriteLine(c);
+    }
+}
